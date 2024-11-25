abebc441be19f20a39f3e1193f96e5ed2fe03950
Christian Lee <chris109182@gmail.com>
2024-11-25 03:13:35 -0500

    import csv

---
+++ CODE/import_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+username = "postgres" #default username, change if you changed
+password = "ace109182" #input password
+host = "localhost" #default
+port = "5432" #default
+# Create a connection to PostgreSQL
+engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/accidents")
+
+# Process the dataset in chunks
+chunk_size = 10000  # Define chunk size
+for chunk in pd.read_csv('./Collisions Dataset.csv', chunksize=chunk_size):
+    # chunk.columns = [c.lower() for c in chunk.columns]  # Normalize column names
+    chunk.to_sql("collisions", engine, if_exists='append', index=False)
+    print(f"Chunk of size {len(chunk)} imported.")
+print("Import complete.")
